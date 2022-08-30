a54fa789e8ca50c9809222408a4fb2236869bafe
Erhan Ertem <eertem@gmail.com>
2022-08-30 19:10:57 +0300

    Refactor functions for open/close states

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 // const btnShowModal = document.querySelector('.show-modal'); //Selects only thr first matching element
 const btnShowModal = document.querySelectorAll('.show-modal'); //Selects all the matching elements in a nodelist array
+const closeModal = function () {
+  modal.classList.add('hidden'); // make modal hide
+  overlay.classList.add('hidden'); //make overlay hide
+}; //functions assigned to variables needs to be declared first before calling them
+const openModal = function () {
+  modal.classList.remove('hidden'); // takes the hidden from its class name to make modal display
+  overlay.classList.remove('hidden'); //make overlay display
+};
 
 console.log(btnShowModal); //it selects only the first one.
 
@@ -13,12 +21,12 @@ for (let i = 0; i < btnShowModal.length; i++) {
   // console.log(btnShowModal[i].textContent); //comsole.logs all the elements' content in the array of as provided by the queryselectorall
   btnShowModal[i].addEventListener('click', function () {
     console.log('Button Clicked');
-    modal.classList.remove('hidden'); // takes the hidden from its class name to make modal display
-    overlay.classList.remove('hidden'); //make overlay display
+    openModal();
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden'); // make modal hide
-  overlay.classList.add('hidden'); //make overlay hide
-});
+// CLOSING MODAL
+//#1.click on close btn on the modal to close the modal
+btnCloseModal.addEventListener('click', closeModal);
+//#2.click on overlay area to close the modal
+overlay.addEventListener('click', closeModal);
