c8aa17145c83cf1387cbe938f7f765d20e713cd1
Erhan Ertem <eertem@gmail.com>
2022-08-30 18:51:16 +0300

    Replace queryselector with queryselectorall to select all matching nodes

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnShowModal = document.querySelector('.show-modal');
+// const btnShowModal = document.querySelector('.show-modal'); //Selects only thr first matching element
+const btnShowModal = document.querySelectorAll('.show-modal'); //Selects all the matching elements in a nodelist array
 
 console.log(btnShowModal); //it selects only the first one.
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent); //comsole.logs all the elements' content in the array of as provided by the queryselectorall
+}
