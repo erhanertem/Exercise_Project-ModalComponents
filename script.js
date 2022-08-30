491046f86df8db5c4de41bb852ea1f8875310f5c
Erhan Ertem <eertem@gmail.com>
2022-08-30 18:46:38 +0300

    Assign query selector variables for easy coding

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelector('.show-modal');
+
+console.log(btnShowModal); //it selects only the first one.
