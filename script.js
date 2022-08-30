e7d52c18752a2ac47db1813514c37d8657325599
Erhan Ertem <eertem@gmail.com>
2022-08-30 19:03:16 +0300

    Add  btn eventlistener and modal closebtn event listeners

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ const btnShowModal = document.querySelectorAll('.show-modal'); //Selects all the
 
 console.log(btnShowModal); //it selects only the first one.
 
+//forloop that sorts thru the nodelist given by the queryselectorall
 for (let i = 0; i < btnShowModal.length; i++) {
   // console.log(btnShowModal[i].textContent); //comsole.logs all the elements' content in the array of as provided by the queryselectorall
   btnShowModal[i].addEventListener('click', function () {
     console.log('Button Clicked');
+    modal.classList.remove('hidden'); // takes the hidden from its class name to make modal display
+    overlay.classList.remove('hidden'); //make overlay display
   });
 }
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden'); // make modal hide
+  overlay.classList.add('hidden'); //make overlay hide
+});
