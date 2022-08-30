94f43749b2b725138bcbaafa881374e2f5cbbb12
Erhan Ertem <eertem@gmail.com>
2022-08-30 19:27:32 +0300

    Adopt a keyboard event for ESC triggered modal close event

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 // const btnShowModal = document.querySelector('.show-modal'); //Selects only thr first matching element
 const btnShowModal = document.querySelectorAll('.show-modal'); //Selects all the matching elements in a nodelist array
+
 const closeModal = function () {
   modal.classList.add('hidden'); // make modal hide
   overlay.classList.add('hidden'); //make overlay hide
@@ -28,5 +29,14 @@ for (let i = 0; i < btnShowModal.length; i++) {
 // CLOSING MODAL
 //#1.click on close btn on the modal to close the modal
 btnCloseModal.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', function () {
+//   closeModal();
+// }); //either above or this , same stuff
 //#2.click on overlay area to close the modal
 overlay.addEventListener('click', closeModal);
+
+//ADD KEYBOARD EVENT for closing modal with ESC BUTTON
+document.addEventListener('keydown', function (e) {
+  console.log(`A key is pressed`);
+  console.log(e); // reports the keyboardevent nodelist which reports the pressed key!
+});
